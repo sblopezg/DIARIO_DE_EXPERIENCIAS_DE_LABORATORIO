1dbe8ba09f99fd819c9aff09b9bef2f905bbe024
sblopezg <111389162+sblopezg@users.noreply.github.com>
2022-11-08 20:01:17 +0100

    Add files via upload

---
+++ 7.0.0/L1+SBL+1104822.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1__S.B.L_1104822
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese edad");
+            string edad = Console.ReadLine();
+            Console.WriteLine("Ingrese carrera");
+            string carrera = Console.ReadLine();
+            Console.WriteLine("Ingrese carne");
+            string carne = Console.ReadLine();
+
+            Console.WriteLine("soy"+ nombre + "tengo "+ edad + "años y estudio la carrera de" + carrera + "mi numero de carne" + carne);
+            Console.ReadKey();
+        }
+    }
+}
