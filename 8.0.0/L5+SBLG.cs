ea2bf63b03797993de3ae1311e2f87b414ccedd7
sblopezg <111389162+sblopezg@users.noreply.github.com>
2022-11-08 20:07:12 +0100

    Add files via upload

---
+++ 8.0.0/L5+SBLG.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finobacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cant;
+            int a, b, c;
+            Console.WriteLine("Ingresar un numero: ");
+            cant = 
+        Int32.Parse(Console.ReadLine());
+            a = 0; b = 1;
+            Console.WriteLine(a + " " + " ");
+            for (int w = 3; w <= cant; w++)
+            }
+        Console.ReadKey();
+        
+
+        }
+    }
+}
